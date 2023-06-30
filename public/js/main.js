2eba6054525845c180c420521b60a7b668dfd587
Pushkaran <pushkaranjps@gmail.com>
2023-07-01 04:01:40 +0530

    first upload

---
+++ public/js/main.js
@@ -0,0 +1,67 @@
+const apiKey="f2eed5e27da1c8bfcc89f7da18161141";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon=document.querySelector(".weather-icon");
+
+
+
+async function checkWeather(city){
+
+    const response = await fetch(apiUrl +city+ `&appid=${apiKey}`);
+    if(response.status == 404){
+        document.querySelector(".error").style.display="block";
+        document.querySelector(".weather").style.display="none";
+
+    }
+    else{
+        var data=await response.json();
+        console.log(data);
+    
+        document.querySelector(".city").innerHTML=data.name;
+        document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+"°c";
+        document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
+        document.querySelector(".wind").innerHTML=data.wind.speed+"km/h";
+    
+    
+        if(data.weather[0].main=="Clouds"){
+            weatherIcon.src="images/weather/clouds.png";
+        }
+        else if(data.weather[0].main=="Clear")
+        {
+            weatherIcon.src="images/weather/clear.png";
+    
+        }
+        else if(data.weather[0].main=="Rain")
+        {
+            weatherIcon.src="images/weather/rain.png";
+    
+        }
+        else if(data.weather[0].main=="Mist")
+        {
+            weatherIcon.src="images/weather/mist.png";
+    
+        }
+        else if(data.weather[0].main=="Drizzle")
+        {
+            weatherIcon.src="images/weather/drizzle.png";
+    
+        }
+    
+    
+        document.querySelector(".weather").style.display="block";
+        document.querySelector(".error").style.display="none";
+    
+
+    }
+   
+
+}
+
+searchBtn.addEventListener("click",()=>{
+    checkWeather(searchBox.value);
+
+});
+
